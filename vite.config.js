a9d7a0375547bf93980a72f52f5f53aef91f752b
Taras Papka <taras.papka@protonmail.com>
2023-10-10 13:31:25 +0300

    main and vite config added base

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/KDMSH',
-  resolve: {
-    alias: {
-      src: './src',
-      components: './src/components',
-    },
-  }
+  base: '/KDMSH',
+  // resolve: {
+  //   alias: {
+  //     src: './src',
+  //     components: './src/components',
+  //   },
+  // }
 });
